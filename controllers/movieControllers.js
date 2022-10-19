b928db7577f55d8563559c4384773f10f0ea06cf
Aan Bahudin <aan.bahudin11@gmail.com>
2022-10-19 11:44:24 +0800

    adding page params

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -4,38 +4,34 @@ const axios = require('axios')
 const baseURL = `https://api.themoviedb.org/3`
 
 const getFeatureMovie = async(req, res) => {
-    const {feature} = req.params
+    const {feature, page} = req.params
     try {
         const url = feature === 'trending' ? `${feature}/movie/week?` : `movie/${feature}?`
-        const response = await axios.get(`${baseURL}/${url}api_key=${process.env.API_KEY}`)
+        const response = await axios.get(`${baseURL}/${url}api_key=${process.env.API_KEY}&page=${page}`)
         res.status(StatusCodes.OK).json(response.data)
     } catch (error) {
         console.log(error);
     }
 }
-
 const getFeatureTv = async(req, res) => {
-    const {feature} = req.params
-    console.log(feature);
+    const {feature, page} = req.params
 
     try {
-        const response = await axios.get(`https://api.themoviedb.org/3/tv/${feature}?api_key=${process.env.API_KEY}`)
+        const response = await axios.get(`https://api.themoviedb.org/3/tv/${feature}?api_key=${process.env.API_KEY}&page=${page}`)
         res.status(StatusCodes.OK).json(response.data)
     } catch (error) {
         console.log(error);
     }
-
-    // try {
-    //     const url = feature === 'trending' ? `${feature}/tv/week?` : `tv/${feature}?`
-    //     const response = await axios.get(`${baseURL}/${url}api_key=${process.env.API_KEY}`)
-    //     res.status(StatusCodes.OK).json(response.data)
-    // } catch (error) {
-    //     console.log(error);
-    // }
 }
 
 const getSingleMovies = async(req, res) => {
-    res.send('get single movies controllers')
+    const {type, id} = req.params
+    try {
+        const response = await axios.get(`${baseURL}/${type}/${id}?api_key=${process.env.API_KEY}`)
+        res.status(StatusCodes.OK).json(response.data)
+    } catch (error) {
+        res.status(StatusCodes.BAD_REQUEST).json({success: false, msg: `can't find movie/tv with id ${id}`})
+    }
 }
 
 const searchMovies = async(req, res) => {
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const { getSingleMovies, searchMovies, getFeatureMovie, getFeatureTv } = require('../controllers/movieControllers')
 
-router.route('/featured/movie/:feature')
+router.route('/featured/movie/:feature/:page')
     .get(getFeatureMovie)
 
-router.route('/featured/tv/:feature')
+router.route('/featured/tv/:feature/:page')
     .get(getFeatureTv)
 
-router.route('/:id')
+router.route('/:type/:id')
     .get(getSingleMovies)
 
 router.route('/search')
