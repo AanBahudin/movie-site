36d2b1695226c07f212de1faf52f1e9745a448c6
Aan Bahudin <aan.bahudin11@gmail.com>
2022-10-21 21:59:46 +0800

    footer fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <main className="py-[3%] bg-crayola flex items-center justify-center font-roboto uppercase px-[5%]">
+        <main className="py-[3%] absolute bottom-0 w-full bg-crayola flex items-center justify-center font-roboto uppercase px-[5%]">
             Movie & Tv Shows Information | 2022
         </main>
     )
--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({trendingMovie, trendingTv}) {
           <title>Movie API - Search Every Movie & TV Shows</title>
         </Head>
 
-        <section className='flex font-roboto uppercase text-xl text-[#F4F4F5] w-[50%] pt-[10%] mx-auto justify-around items-center'>
+        {/* <section className='flex font-roboto uppercase text-xl text-[#F4F4F5] w-[50%] pt-[10%] mx-auto justify-around items-center'>
           <Link href='/media/tv'><a>Tv Show</a></Link> <Link href='/media/movie'><a>Movies</a></Link>
         </section>
 
@@ -42,7 +42,7 @@ export default function Home({trendingMovie, trendingTv}) {
           {HomeCardContainerData.map((item, item_id) => {
               return <HomeCardContainer key={item_id} path={item.path} id_name={item.id_name} title={item.title} result={results[item_id]} urlPath={searchPath[item_id]} page={1} />
           })}
-        </>
+        </> */}
         
         <Footer />
       </main>
